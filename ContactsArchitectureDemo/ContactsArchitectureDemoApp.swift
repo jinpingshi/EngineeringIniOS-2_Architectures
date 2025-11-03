19be1d92ea3655e79c04310a23caac3d43d1a90f
JinpingShi <jinping_shi@epam.com>
2025-11-03 11:21:25 +0800

    Initial Commit

---
+++ ContactsArchitectureDemo/ContactsArchitectureDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsArchitectureDemoApp.swift
+//  ContactsArchitectureDemo
+//
+//  Created by Jinping Shi on 2025/11/3.
+//
+
+import SwiftUI
+
+@main
+struct ContactsArchitectureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactsArchitectureDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContactsArchitectureDemo
+//
+//  Created by Jinping Shi on 2025/11/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
